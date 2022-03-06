0e5a17c0d1610750876bce1d68c08b5fd22bb864
malikdemaximus <malikdemaximus@mail.ru>
2022-03-06 13:22:37 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './index.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import DUMMY_NOTES from './DUMMY_NOTES';
+import Note from './components/Note/Note';
+import INote from './interfaces/note.interface';
 
 function App() {
+  const [notesList, setNotesList] = useState<any[]>([]);
+
+  useEffect(() => {
+    const listFromStorage = localStorage.getItem("my-notes");
+    if (listFromStorage) {
+      const listFromStorageArray = JSON.parse(listFromStorage);
+      setNotesList(listFromStorageArray);
+    } else {
+      setNotesList(DUMMY_NOTES);
+    }
+  }, []);
+
+  useEffect(() => {
+    const notesListString = JSON.stringify(notesList);
+    localStorage.setItem('my-notes', notesListString);
+  }, [notesList])
+
+  // const getNotes = async () => {
+  //   try {
+  //     const response = await axios.get("http://localhost:5000/notes");
+  //     setNotesList(response.data.notes);
+  //     console.log(notesList);
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // }
+
+  const updateNoteItem = (updatedNote: INote) => {
+    const updatedList = notesList.map((noteItem: INote) => {
+      if (noteItem._id === updatedNote._id) {
+        return updatedNote
+      }
+      return noteItem
+    })
+    setNotesList(updatedList);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="notes-list">
+        {
+          notesList.map((noteItem, index) => {
+            return <Note note={noteItem} onNoteUpdate={updateNoteItem} key={index} />
+          })
+        }
+      </div>
     </div>
   );
 }
---
+++ src/components/Note/Note.tsx
@@ -0,0 +1,37 @@
+import { FC, FocusEvent } from 'react';
+import INote from "../../interfaces/note.interface"
+import "./Note.css"
+
+type Props = {
+  note: INote,
+  onNoteUpdate: (note: INote) => void,
+}
+
+const Note: FC<Props> = ({ note, onNoteUpdate }) => {
+  const noteTextUpdated = (event: FocusEvent<HTMLDivElement>) => {
+    const newTextValue = event.currentTarget.textContent;
+    if (newTextValue === note.text) {
+      return;
+    }
+    const updatedNoteObject = {
+      ...note,
+      text: newTextValue || ''
+    };
+    onNoteUpdate(updatedNoteObject);
+  }
+
+  return (
+    <div className="note">
+      <div
+        onBlur={noteTextUpdated}
+        contentEditable={true}
+        suppressContentEditableWarning={true}
+        className="note__text">{note.text}</div>
+      <div className="note__link">
+        <a href={note.link}>{note.link}</a>
+      </div>
+    </div>
+  )
+}
+
+export default Note
